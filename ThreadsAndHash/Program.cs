757f6631e47a4b9cffce38d2e82d2d035cac98dc
GIM <GIM@GIM->
2017-04-11 19:30:48 +0300

    Add project files.

---
+++ ThreadsAndHash/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ThreadsAndHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = args[0];
+            string partSize = args[1];
+            
+            InputFile file = new InputFile(path, partSize);
+
+            file.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
